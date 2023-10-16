3cbd7f2b7b0d77ae6d256aee8f245068e579b266
rogulik <malvla2008@gmail.com>
2023-10-16 20:20:58 +0300

    React.ts project initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div className="container">
+      This is main container
+    </div>
+  )
+}
+
+export default App
